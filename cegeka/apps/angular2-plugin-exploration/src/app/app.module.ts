9894f0764a53e966d69eaad347065a0ff83ab266
nielsjani <niels.jani@cegeka.com>
2022-06-23 21:16:41 +0200

    add routing

--- cegeka/apps/angular2-plugin-exploration/src/app/app.module.ts
+++ cegeka/apps/angular2-plugin-exploration/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent, HomeComponent } from '@cegeka/home';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent }
+];
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
---
+++ cegeka/libs/home/src/index.ts
@@ -0,0 +1,5 @@
+export * from './lib/home.module';
+
+export * from './lib/home/home.component';
+
+export * from './lib/about/about.component';
