fdfb3a906320aaed06d424c16fc5be5b7c3af9e4
nielsjani <niels.jani@cegeka.com>
2022-06-24 13:07:08 +0200

    pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes pipes (and fix merge)

--- cegeka/apps/angular2-plugin-exploration/src/app/app.module.ts
+++ cegeka/apps/angular2-plugin-exploration/src/app/app.module.ts
@@ -1,12 +1,18 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {HatoolLibModule} from "hatool";
+import { HatoolLibModule } from 'hatool';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule, Routes } from '@angular/router';
-import { ChartsComponent, HomeComponent, PijpenComponent, TreesComponent, ChatbotComponent,UserIdleComponent } from '@cegeka/home';
-import { HttpClientModule } from '@angular/common/http';
+import {
+  ChartsComponent,
+  ChatbotComponent,
+  HomeComponent,
+  PijpenComponent,
+  TreesComponent,
+  UserIdleComponent
+} from '@cegeka/home';
 import { NgIdleKeepaliveModule } from '@ng-idle/keepalive';
 
 const routes: Routes = [
@@ -14,8 +20,7 @@ const routes: Routes = [
   { path: 'charts', component: ChartsComponent },
   { path: 'tree', component: TreesComponent },
   { path: 'user-idle', component: UserIdleComponent },
-  {path: 'chatbot', component: ChatbotComponent}
-  { path: 'user-idle', component: UserIdleComponent },
+  {path: 'chatbot', component: ChatbotComponent},
   { path: 'pipes', component: PijpenComponent }
 ];
 
